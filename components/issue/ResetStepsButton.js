a2a3bc8137dde2c1ccf62c75906e5d02a082e871
Ahmad-B-Mahmoud <ahmadmahmoud.webdev@protonmail.com>
2022-11-21 03:45:06 +0300

    Add Reset steps button.

---
+++ components/issue/ResetStepsButton.js
@@ -0,0 +1,52 @@
+import { Box, Button } from "@mui/material";
+import { RestartAlt } from "@mui/icons-material";
+import { useResetStep } from "context/Steps";
+import { useUpdateIssue } from "context/Issue";
+
+function ResetStepsButton() {
+  // Using the custom Hook to update the Issue general state
+  const updateIssue = useUpdateIssue();
+
+  // Use next step context
+  const goResetSteps = useResetStep();
+
+  // Reset Handler
+  const resetSteps = () => {
+    const data = {
+      gender: "",
+      amount: 0,
+      creed: "",
+      issueType: "",
+      allStocks: 0,
+      fardInherits: [],
+      leagueInherits: [],
+      note: "",
+      hasSons: false,
+      hasChild: false,
+      hasPartner: false,
+    };
+    // Reset Issue Context
+    updateIssue(data);
+
+    // Reset Steps Context
+    goResetSteps();
+  };
+
+  return (
+    <Box textAlign="center" sx={{ mb: 2 }}>
+      <div>
+        <Button
+          variant="outlined"
+          onClick={resetSteps}
+          sx={{ mt: 6, mr: 1 }}
+          size="large"
+        >
+          <RestartAlt />
+          مسألة جديدة
+        </Button>
+      </div>
+    </Box>
+  );
+}
+
+export default ResetStepsButton;
