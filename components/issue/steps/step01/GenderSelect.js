dac5a87de7f430e07c30c1b2085ef7b162669733
Ahmad-B-Mahmoud <ahmadmahmoud.webdev@protonmail.com>
2022-11-21 03:48:32 +0300

    Change Gender text color.

--- components/issue/steps/step01/GenderSelect.js
+++ components/issue/steps/step01/GenderSelect.js
@@ -11,8 +11,8 @@ function GenderSelect({gender, genderChange}) {
         value={gender.value}
         required 
         onChange={(e) => {genderChange({value: e.target.value, error: false})}}>
-        <FormControlLabel value="male" control={<Radio />} label={<Typography fontSize="1.2rem" fontWeight="bold" color="#fffe"><MaleTwoTone fontSize="inherit" /> ذكر</Typography>} />
-        <FormControlLabel value="female" control={<Radio />} label={<Typography fontSize="1.2rem" fontWeight="bold" color="#fffe"><FemaleTwoTone fontSize="inherit" /> إنثى</Typography>} />
+        <FormControlLabel value="male" control={<Radio />} label={<Typography fontSize="1.2rem" fontWeight="bold" color="secondary"><MaleTwoTone fontSize="inherit" /> ذكر</Typography>} />
+        <FormControlLabel value="female" control={<Radio />} label={<Typography fontSize="1.2rem" fontWeight="bold" color="secondary"><FemaleTwoTone fontSize="inherit" /> إنثى</Typography>} />
       </RadioGroup>
       <FormHelperText>{gender.error ? 'يرجى إختيار جنس صاحب الإرث' : null}</FormHelperText>
     </FormControl>
